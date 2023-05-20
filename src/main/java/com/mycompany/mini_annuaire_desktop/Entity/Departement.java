e3f1c73bc92a04ea7b677d0b5484f360384de7c9
kaoutar boudouft <kaoutarboudouft2@gmail.com>
2023-05-20 22:26:45 +0100

    initialize project

---
+++ src/main/java/com/mycompany/mini_annuaire_desktop/Entity/Departement.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mini_annuaire_desktop.Entity;
+
+
+/**
+ *
+ * @author KAOUTAR
+ */
+public class Departement {
+    private String label;
+
+    public Departement(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }   
+    
+    
+}
+
+
